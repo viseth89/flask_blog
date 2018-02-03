49d38136928e6862d2910cb3f3a390f11efbbe0c
root <root@ubuntu-s-1vcpu-1gb-nyc1-01.localdomain>
2018-02-03 01:01:33 +0000

    added comment for local db file

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/www/FlaskApp/FlaskApp/blog.db'
-
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/www/FlaskApp/FlaskApp/blog.db'
 db = SQLAlchemy(app)
 
 class Blogpost(db.Model):
